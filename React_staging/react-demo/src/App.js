0c5008a776f51aff7bf45cc8fd3891dfae728a53
zhou_oyster <zhou_oyster@qq.com>
2021-02-22 09:02:52 +0800

    react-2021-02-22

--- React_staging/react-demo/src/App.js
+++ React_staging/react-demo/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import axios  from 'axios';
+export default class App extends Component {
+  handleNetwork1 = ()=>{
+    axios.get('http://localhost:3000/api1/students').then(res=>{
+      console.log(res);
+    },err=>{
+      console.log(err);
+    })
+  }
+  handleNetwork2 = ()=>{
+    axios.get('http://localhost:3000/api2/cars').then(res=>{
+      console.log(res);
+    },err=>{
+      console.log(err);
+    })
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleNetwork1}>获取学生信息</button>
+        <button onClick={this.handleNetwork2}>获取汽车信息</button>
+      </div>
+    )
+  }
 }
 
-export default App;
