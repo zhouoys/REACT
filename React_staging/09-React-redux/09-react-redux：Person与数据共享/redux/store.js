b7cbb693c7527285e406802a873df6ec83c288d4
zhou_oyster <zhou_oyster@qq.com>
2021-05-17 08:46:51 +0800

    提交

--- React_staging/09-React-redux/09-react-redux：Person与数据共享/redux/store.js
+++ React_staging/09-React-redux/09-react-redux：Person与数据共享/redux/store.js
@@ -6,8 +6,8 @@ import {createStore,applyMiddleware,combineReducers} from 'redux'
 //引入redux-thunk，用于持支异步actions
 import thunk from 'redux-thunk';
 //引入为Counter组件服务的reducer
-import counterReducer from '../redux/reducer/counter.js'
-import personReducer from '../redux/reducer/person.js'
+import counterReducer from './reducer/counter.js'
+import personReducer from './reducer/person.js'
 //汇总所有的reducer,成为一个allReducer
 const allReducer = combineReducers({
     count:counterReducer, //key为当前的reducer所传递的状态名称，值为容器组件对应的reducer
